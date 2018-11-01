a8539884fc01c4ddde5e781837d44e42bea1fd11
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-01 15:14:54 +0100

    Adds putFoodInMouth(food) to Animal

--- Animal.js
+++ Animal.js
@@ -4,6 +4,12 @@ class Animal {
     constructor (species, legs) {
         this.species = species
         this.legs = legs
+        
+        this.mouth = []
+    }
+
+    putFoodInMouth(food) {
+        this.mouth.push(food)
     }
 }
 
